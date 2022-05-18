5d5106dd15c6db3012ed1f698df3bc6424d00d6c
Duedot43 <jackgendill@icloud.com>
2022-05-18 12:04:17 -0600

    Fixed the likes

--- Posts/like.php
+++ Posts/like.php
@@ -3,13 +3,16 @@ $post = $_GET['post'];
 $index = json_decode(file_get_contents("../index.json"), true);
 $like_id = uniqid();
 foreach ($index['posts'][$post]['likes'] as $likes_array){
-    if(in_array($_COOKIE['user-id'], $likes_array)){
+    if(in_array($_COOKIE['user_id'], $likes_array)){
+        unset($index['posts'][$post]['likes'][$likes_array['like-id']]);
+        file_put_contents("../index.json", json_encode($index));
         header("Location: /");
         exit();
     }
 }
 $index['posts'][$post]['likes'][$like_id] = array(
-    $_COOKIE['user_id']
+    $_COOKIE['user_id'],
+    "like-id"=>$like_id
 );
 file_put_contents("../index.json", json_encode($index));
 header("Location: /");
