443d980634ca4ac7ccd2c39bb782d14d274382b6
Duedot43 <jackgendill@icloud.com>
2022-05-18 09:12:28 -0600

    It works!

---
+++ Posts/add.php
@@ -0,0 +1,49 @@
+<?php
+if (isset($_POST['context'])){
+    $index = json_decode(file_get_contents("../index.json"), true);
+    $post_id = uniqid();
+    $index['posts'][$post_id] = array(
+        "post-id"=>$post_id,
+        "img"=>"/Posts/" . $post_id . ".png",
+        "context"=>$_POST['context'],
+        "write"=>$_COOKIE['user_id'],
+        "comments"=>array()
+    );
+    array_push($index['users'][$_COOKIE['user_id']]['posts'], $post_id);
+    file_put_contents("../index.json", json_encode($index));
+    move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "./" . $post_id . ".png");
+    header("Location: /");
+}
+
+?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Register</title>
+<tr>
+    <form method="post" name="form" enctype="multipart/form-data" action="/Posts/add.php">
+        <td>
+            <table width="100%" border="0" cellpadding="3" cellspacing="1">
+                <tr>
+                    <td colspan="3"><strong>Make a post
+                            <hr />
+                        </strong></td>
+                </tr>
+                <tr>
+                    <td class="text" width="78">Context
+                        <td width="6">:</td>
+                        <td width="294"><input class="box" name="context" type="text" id="context" autocomplete="off" required></td>
+                    </td>
+                </tr>
+                <tr>
+                    <td><input type="file" name="fileToUpload" id="fileToUpload">Upload a profile picture</td>
+                </tr>
+</tr>
+<tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><input class="reg" type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 $index = json_decode(file_get_contents("index.json"), true);
 if (!isset($_COOKIE['user_id']) or !isset($_COOKIE['token'])){
-    echo ('11<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
+    echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
     exit();
 }
 if(!isset($index['users'][$_COOKIE['user_id']]) or $index['users'][$_COOKIE['user_id']]['token'] != $_COOKIE['token']){
-    echo ('22<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
+    echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
     exit();
 }
-echo "<link href='/style.css' rel='stylesheet' type='text/css'/> <img src='" . $index['users'][$_COOKIE['user_id']]['pfp'] . "' width='50' class='profile' height=auto\><br>Welcome, " . $index['users'][$_COOKIE['user_id']]['user-name'] . "<br>";
+echo "<link href='/style.css' rel='stylesheet' type='text/css'/> <img src='" . $index['users'][$_COOKIE['user_id']]['pfp'] . "' width='50' class='profile' height=auto\><br>Welcome, " . $index['users'][$_COOKIE['user_id']]['user-name'] . "<br><img src='https://cdn.onlinewebfonts.com/svg/img_28497.png' width='50'  height=auto onclick='location=\"/Posts/add.php\"'/><br>";
 foreach ($index['posts'] as $post_array){
     echo "
-    <link href='/style.css' rel='stylesheet' type='text/css'/>
-    " . $index['users'][$post_array['write']]['user-name'] . "<br>
-    <img src='" . $index['users'][$post_array['write']]['pfp'] . "' width='50' class='profile' height=auto/>
+    <img onclick='location=\"/Users/posts.php?user=" . $index['users'][$post_array['write']]['user-id'] . "\"' src='" . $index['users'][$post_array['write']]['pfp'] . "' width='50' class='profile' height=auto/>" . $index['users'][$post_array['write']]['user-name'] . "<br>
     " .$post_array['context'] . "<br>
-    <img src='" . $post_array['img'] . "' class='big_post' />
+    <img src='" . $post_array['img'] . "' class='big_post' /><br>
     ";
 }
 ?>
