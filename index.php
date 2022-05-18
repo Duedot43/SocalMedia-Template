acd6221a8ce8f8439a20e4b33a0176a0c4ca10e0
Duedot43 <jackgendill@icloud.com>
2022-05-17 23:42:54 -0600

    mmm

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ if(!isset($index['users'][$_COOKIE['user_id']]) or $index['users'][$_COOKIE['use
     echo ('22<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
     exit();
 }
-echo "<link href='/style.css' rel='stylesheet' type='text/css'/> <img src='" . $index['users'][$_COOKIE['user_id']]['pfp'] . "' width='50' class='profile' height=auto\><br>Welcome, " . $index['users'][$_COOKIE['user_id']]['user-name'];
-
+echo "<link href='/style.css' rel='stylesheet' type='text/css'/> <img src='" . $index['users'][$_COOKIE['user_id']]['pfp'] . "' width='50' class='profile' height=auto\><br>Welcome, " . $index['users'][$_COOKIE['user_id']]['user-name'] . "<br>";
+foreach ($index['posts'] as $post_array){
+    echo "
+    <link href='/style.css' rel='stylesheet' type='text/css'/>
+    " . $index['users'][$post_array['write']]['user-name'] . "<br>
+    <img src='" . $index['users'][$post_array['write']]['pfp'] . "' width='50' class='profile' height=auto/>
+    " .$post_array['context'] . "<br>
+    <img src='" . $post_array['img'] . "' class='big_post' />
+    ";
+}
 ?>
