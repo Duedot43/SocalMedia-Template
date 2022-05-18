5bcd4a255c98304ba052085d344726f0bf2e33de
Duedot43 <jackgendill@icloud.com>
2022-05-17 21:45:25 -0600

    Time to test the login system

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 $index = json_decode(file_get_contents("index.php"), true);
-
+if (!isset($_COOKIE['user_id']) or !isset($_COOKIE['token'])){
+    echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
+    exit();
+}
+if(!isset($index['users'][$_COOKIE['user_id']]) and $index['users'][$_COOKIE['user_id']]['token'] != $_COOKIE['token']){
+    echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
+    exit();
+}
 ?>
-<button onclick="location='/Users/login.php'">Login</button><button onclick="location='/Users/sign-up.php'">Sign up</button>
