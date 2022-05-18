8bfe8669410f13841cab32fff13809754865252d
Duedot43 <jackgendill@icloud.com>
2022-05-18 12:13:16 -0600

    Better following system

--- Users/follow.php
+++ Users/follow.php
@@ -1,9 +1,22 @@
 <?php
 $user = $_GET['user'];
 $index = json_decode(file_get_contents("../index.json"), true);
-array_push($index['users'][$user]['followers'], $_COOKIE['user_id']);
-array_push($index['users'][$_COOKIE['user_id']]['following'], $user);
-file_put_contents("../index.json", json_encode($index));
-header("Location: /Users/posts.php?user=" . $user);
-exit();
+if(!in_array($user, $index['user'][$_COOKIE['user_id']]['following'])){
+    $foll = "Follow";
+} else{
+    $foll = "Unfollow";
+}
+if($foll == "Follow"){
+    array_push($index['users'][$user]['followers'], $_COOKIE['user_id']);
+    array_push($index['users'][$_COOKIE['user_id']]['following'], $user);
+    file_put_contents("../index.json", json_encode($index));
+    header("Location: /Users/posts.php?user=" . $user);
+    exit();
+} else{
+    unset($index['users'][$user]['followers'][$_COOKIE['user_id']]);
+    unset($index['users'][$_COOKIE['user_id']]['following'][$user]);
+    file_put_contents("../index.json", json_encode($index));
+    header("Location: /Users/posts.php?user=" . $user);
+    exit();
+}
 ?>
--- Users/posts.php
+++ Users/posts.php
@@ -1,14 +1,19 @@
 <?php
 $user = $_GET['user'];
 $index = json_decode(file_get_contents("../index.json"), true);
-echo "<link href='/style.css' rel='stylesheet' type='text/css'/><img src='" . $index['users'][$user]['pfp'] . "' width='100' class='profile' height=auto/>" . $index['users'][$user]['user-name'] . "<br><button onclick='location=\"/Users/follow.php?user=" . $index['users'][$user]['user-id'] . "\"'>Follow</button><br>";
+if(!in_array($user, $index['user'][$_COOKIE['user_id']]['following'])){
+    $foll = "Follow";
+} else{
+    $foll = "Unfollow";
+}
+echo "<link href='/style.css' rel='stylesheet' type='text/css'/><img src='" . $index['users'][$user]['pfp'] . "' width='100' class='profile' height=auto/>" . $index['users'][$user]['user-name'] . "<br><button onclick='location=\"/Users/follow.php?user=" . $index['users'][$user]['user-id'] . "\"'>" . $foll . "</button><br>";
 foreach ($index['posts'] as $post_array){
-    if ($post_array['write'] == $user){
-        echo "
-        <img onclick='location=\"/Users/posts.php?user=" . $index['users'][$post_array['write']]['user-id'] . "\"' src='" . $index['users'][$post_array['write']]['pfp'] . "' width='50' class='profile' height=auto/>" . $index['users'][$post_array['write']]['user-name'] . "<br>
-        " .$post_array['context'] . "<br>
-        <img src='" . $post_array['img'] . "' class='big_post' width='800' height=auto/><br>
-        ";
-    }
+    echo "
+    <img onclick='location=\"/Users/posts.php?user=" . $index['users'][$post_array['write']]['user-id'] . "\"' src='" . $index['users'][$post_array['write']]['pfp'] . "' width='50' class='profile' height=auto/>" . $index['users'][$post_array['write']]['user-name'] . "<br>
+    " .$post_array['context'] . "<br>
+    <img src='" . $post_array['img'] . "' class='big_post' width='800' height=auto/><br>
+    <img src='https://www.freeiconspng.com/uploads/comment-png-3.png' width='30' height='auto' onclick='location=\"/Posts/comment.php?post=" . $post_array['post-id'] . "\"'/>" . count($post_array['comments']) . "
+    <img src='https://image.similarpng.com/very-thumbnail/2020/06/Icon-like-button-transparent-PNG.png' width='30' height='auto' onclick='location=\"/Posts/like.php?post=" . $post_array['post-id'] . "\"'/>" . count($post_array['likes']) . "
+    ";
 }
 ?>
