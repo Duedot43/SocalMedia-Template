84820b250a705382a308a8be5f5ec8ec4bfbcd0c
Duedot43 <jackgendill@icloud.com>
2022-05-17 21:48:57 -0600

    Or makes sense

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (!isset($_COOKIE['user_id']) or !isset($_COOKIE['token'])){
     echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
     exit();
 }
-if(!isset($index['users'][$_COOKIE['user_id']]) and $index['users'][$_COOKIE['user_id']]['token'] != $_COOKIE['token']){
+if(!isset($index['users'][$_COOKIE['user_id']]) or $index['users'][$_COOKIE['user_id']]['token'] != $_COOKIE['token']){
     echo ('<button onclick="location=\'/Users/login.php\'">Login</button><button onclick="location=\'/Users/sign-up.php\'">Sign up</button>');
     exit();
 }
